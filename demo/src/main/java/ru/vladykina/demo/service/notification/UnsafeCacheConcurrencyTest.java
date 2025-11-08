9d97285485b0a5cdd4f37e02298faf9ab21e85a8
Anastttasia <nastya.kaneva.99@gmail.com>
2025-11-08 21:17:20 +0300

    lab 12

---
+++ demo/src/main/java/ru/vladykina/demo/service/notification/UnsafeCacheConcurrencyTest.java
@@ -0,0 +1,70 @@
+package ru.vladykina.demo.service.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.annotation.DirtiesContext;
+import ru.vladykina.demo.BaseIntegrationTest;
+import ru.vladykina.demo.cache.TransactionCache;
+import ru.vladykina.demo.data.TransferGeneratorService;
+import ru.vladykina.demo.service.CacheableService;
+import ru.vladykina.demo.service.exproblem.StreamTransferService;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DirtiesContext
+public class UnsafeCacheConcurrencyTest extends BaseIntegrationTest {
+
+    @Qualifier("concurrentTransactionCache")
+    @Autowired
+    private TransactionCache cache;
+
+    @Autowired
+    private StreamTransferService streamTransferService;
+
+    private static final int THREAD_COUNT = 50;
+
+    @Test
+    void testUnsafeHashMapCacheThrowsConcurrentModificationException() throws Exception {
+        // Запускаем нагрузку на небезопасный кэш
+        AtomicReference<Throwable> exceptionRef = new AtomicReference<>();
+
+        CacheableService service = new CacheableService(cache);
+        List<TransferGeneratorService.TransferOperation> transfers = streamTransferService.getOperations();
+
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
+        CountDownLatch latch = new CountDownLatch(transfers.size());
+
+        for (TransferGeneratorService.TransferOperation op : transfers) {
+            executor.submit(() -> {
+                try {
+                    service.executeTransfer(op);
+                } catch (Throwable t) {
+                    // Запоминаем первую ошибку
+                    exceptionRef.compareAndSet(null, t);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await(10, TimeUnit.SECONDS); // ждём завершения или таймаута
+        executor.shutdown();
+
+        // Проверяем: если была ошибка — это ожидаемо для небезопасного кэша
+        Throwable error = exceptionRef.get();
+//        assertTrue(error instanceof ConcurrentModificationException ||
+//                error.getCause() instanceof ConcurrentModificationException);
+        assertTrue(error==null);
+        //assertTrue(error.getClass().getSimpleName().contains("ConcurrentModificationException"))
+        //
+
+    }
+}

896164474ff290e4bdfbd46bb9982e02f110a553
Anastttasia <nastya.kaneva.99@gmail.com>
2025-11-08 21:19:18 +0300

    lab 13

---
+++ demo/src/main/java/ru/vladykina/demo/controller/DemoVirtualThreadController.java
@@ -0,0 +1,17 @@
+package ru.vladykina.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.vladykina.demo.service.DemoVirtualThreadService;
+
+@RestController
+@RequiredArgsConstructor
+public class DemoVirtualThreadController {
+    private final DemoVirtualThreadService service;
+
+    @GetMapping("/api/demo/vt")
+    public String demo(){
+        return service.demo();
+    }
+}
